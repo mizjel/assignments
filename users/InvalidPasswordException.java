35091b9af0974ee2fc992591387078d62aa4ef84
mizjel <mizjel@gmail.com>
2020-11-24 12:30:34 +0100

    Delete InvalidPasswordException.java

--- users/InvalidPasswordException.java
+++
@@ -1,9 +0,0 @@
-package nl.sogyo.javaopdrachten;
-
-public class InvalidPasswordException extends Exception {
-
-    private static final long serialVersionUID = 1L;
-    public InvalidPasswordException(String errorMessage) {
-        super(errorMessage);
-    }
-}
